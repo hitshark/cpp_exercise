824c7227326faa2f212d10494eafaa91a5a856af
litao <tao.li@deephi.tech>
2018-08-24 21:02:41 +0800

    add item7

---
+++ effective_cpp_3/item7.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class VirtualDestructor {
+    public:
+        VirtualDestructor(uint32_t age_ = 10, uint32_t score_ = 0) : age(age_), score(score_) {}
+        //virtual ~VirtualDestructor(){}
+        ~VirtualDestructor(){}
+    private:
+        uint32_t age;
+        uint32_t score;
+};
+
+void disp(uint64_t dat) {std::cout<<dat<<std::endl;}
+
+int main()
+{
+    VirtualDestructor a;
+    std::cout<<sizeof(a)<<std::endl;
+    disp(*reinterpret_cast<uint64_t *>(&a));
+
+    return 0;
+}
